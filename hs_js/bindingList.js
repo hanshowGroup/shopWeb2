b224cef2a0e2c80ce619d30f91f9acdefdc2c5ac
ltan <li.tan@hanshows.com>
2015-12-10 13:46:32 +0800

    表格的数据加载

--- hs_js/bindingList.js
+++ hs_js/bindingList.js
@@ -1,14 +1,46 @@
-$(document).ready(function() {
-//    $('#apTable').dataTable ({
-//        
-//    });
-    $('.btn-binding').click(function(e){
-		e.preventDefault();
-		$('#bindingGoods').modal('show');
-	});
-    
-     $('.btn-unbinding').click(function(e){
-		e.preventDefault();
-		$('#unbindingGoods').modal('show');
-	});
-});
+ var app = angular.module('bindinglistApp', []);
+     app.controller('bindinglistCtrl', function($scope) {
+	    $scope.error=true;
+		/*/!* var bindbtn=true;*!/
+		 $scope.mouseleave = function () {
+			 bingbtn=true;
+		 }
+		 $scope.mouseenter=function(){
+			 bindbtn=false;
+		 }*/
+		 $scope.editbindingList=function(){
+			 $scope.error=false;
+		 }
+		/*$scope.bodyonclick=function(){
+			 if(bindbtn){
+					 $scope.error=bindbtn;
+
+			 }else{
+				  $scope.error=bindbtn;
+				 }
+			 }*/
+
+		/* if(bindbtn){
+			 $scope.editbindingList=function(){
+				 $scope.error=bindbtn;
+			 }
+		 }else{
+			 $scope.editbindingList=function(){
+				 $scope.error=bindbtn;
+			 }
+		 }*/
+	 });
+ $(document).ready(function() {
+	/* $('. row-binding').click(function (e){
+		 e.preventDefault();
+		 $('.btn-binding').prop(disable, true);
+	 });*/
+	 $('.btn-binding').click(function(e){
+		 e.preventDefault();
+		 $('#bindingGoods').modal('show');
+	 });
+	 $('.btn-unbinding').click(function(e){
+		 e.preventDefault();
+		 $('#unbindingGoods').modal('show');
+	 });
+ });
--- hs_js/eslList.js
+++ hs_js/eslList.js
@@ -2,57 +2,195 @@
  * Created by admin on 2015/12/5.
  */
 var app = angular.module('esllistApp', []);
- app.controller('esllistCtrl',[ function($scope, $http) {
-    /* console.log($scope.esllists);*/
-        $scope.get= function () {
-         $http.get("/shop-web/json/eslList.json")
+ app.controller('esllistCtrl',function($scope) {
+     /* console.log($scope.esllists);*/
+     /*  $scope.get= function () {*/
+     // $scope.esllists=null;
+     $scope.editEsllit = function (id) {
+         $scope.goodId = $scope.esllists[id - 1].goodId;
+         $scope.basestation = $scope.esllists[id - 1].basestation;
+         $scope.eslId = $scope.esllists[id - 1].id;
+     }
+     /* $http.get("/shop-web/json/eslList.json")
              .success(function (response) {
-                 console.log(response.esllists);
                  $scope.esllists = response.esllists;
                  // return response.esllists;
-//            console.log($scope.esllists);
-                 // var $log =$http.get();
-                 /* $log.debug(response.esllists + $filter("json")(target));*/
-             });
-         /* $scope.editEsllit = function(id) {
-          console.log(id);
-          $scope.goodId=$scope.esllists[id-1].goodId;
-          $scope.basestation=$scope.esllists[id-1].basestation;
-          $scope.eslId = $scope.esllists[id-1].id;
-          console.log($scope.eslId);
-          console.log( $scope.goodId);
-          console.log( $scope.basestation);
-          }*/
-     };
-          $scope.get();
-    /* $scope.esllists=[{
-         "shop": "No.001",
-         "id"	   : "01",
-         "type": "11",
-         "size"	   : "11",
-         "electricity":100 ,
-         "bind":100 ,
-         "goodname":"红富士苹果",
-         "goodId":1,
-         "heartbeat":"123" ,
-         "updemode":"已经更新",
-         "updatetime":"2015/11/1 08:00:00",
-         "basestation":111
-     },{
-         "shop": "No.001",
-         "id"	   : "01",
-         "type": "11",
-         "size"	   : "11",
-         "electricity":100 ,
-         "bind":100 ,
-         "goodname":"红富士苹果",
-         "goodId":1,
-         "heartbeat":"123" ,
-         "updemode":"已经更新",
-         "updatetime":"2015/11/1 08:00:00",
-         "basestation":111
-     }];*/
-}
-]);
+                 console.log($scope.esllists);
+             })*/
+         // var $log =$http.get();
+         /* $log.debug(response.esllists + $filter("json")(target));*/
+
+         /*   );
+          /*  } ,2000);*/
+         /*);*/
+         /* };
+          $scope.get();*/
+
+         /*  };*/
+         /*$scope.get();*/
+
+         /*  ]);*/
+        
+         $scope.esllists = [{
+          "shop": "No.001",
+          "id": "01",
+          "type": "11",
+          "size": "11",
+          "electricity": 100,
+          "bind": 100,
+          "goodname": "红富士苹果",
+          "goodId": 1,
+          "heartbeat": "123",
+          "updemode": "已经更新",
+          "updatetime": "2015/11/1 08:00:00",
+          "basestation": 111
+          }, {
+          "shop": "No.001",
+          "id": "02",
+          "type": "11",
+          "size": "11",
+          "electricity": 100,
+          "bind": 100,
+          "goodname": "红富士苹果",
+          "goodId": 2,
+          "heartbeat": "1234",
+          "updemode": "已经更新",
+          "updatetime": "2015/11/1 08:00:00",
+          "basestation": 112
+          },
+             {
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "12345",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             },{
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "1234546",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             },{
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "1234567",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             },{
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "12345678",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             },{
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "123321",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             },{
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "123212",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             },{
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "123344",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             },{
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "123434",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             },{
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "123546",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             },{
+                 "shop": "No.001",
+                 "id": "01",
+                 "type": "11",
+                 "size": "11",
+                 "electricity": 100,
+                 "bind": 100,
+                 "goodname": "红富士苹果",
+                 "goodId": 1,
+                 "heartbeat": "12354",
+                 "updemode": "已经更新",
+                 "updatetime": "2015/11/1 08:00:00",
+                 "basestation": 111
+             }]
+
+ });
+
 
 
